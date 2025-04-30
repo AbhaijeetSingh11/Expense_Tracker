5c7f58a07b6f0f98c5c1bb2ca2c428a3236ba7dd
ABHAIJEET SINGH <abhaijeet11@gmail.com>
2025-05-01 01:53:11 +0530

    vite.config.js updated

--- frontend/expense-tracker/vite.config.js
+++ frontend/expense-tracker/vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    tailwindcss(),
+    react({
+      include: '**/*.jsx',
+    }),
+  ],
   server: {
     open: true,
     port: 5173,
