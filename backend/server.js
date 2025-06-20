e245c12653848967ec7f8badbbcd768212f1a608
ABHAIJEET SINGH <abhaijeet11@gmail.com>
2025-06-20 23:08:38 +0530

    vercel backend server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const dashboardRoutes = require("./routes/dashboardRoutes");
 
 app.use(
     cors({
-        origin: process.env.CLIENT_URL || "*",
+        origin: process.env.CLIENT_URL,
         methods: ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"],
     })
